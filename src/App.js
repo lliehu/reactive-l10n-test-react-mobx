eaca6dd678dc2872d22c575976555250d48de790
Lasse Liehu <lasse.liehu@iki.fi>
2019-11-02 22:08:48 +0200

    Move messages to a separate file

--- src/App.js
+++ src/App.js
@@ -5,17 +5,7 @@ import { observer } from 'mobx-react';
 import {IntlProvider} from 'react-intl';
 import MessageLog from './MessageLog';
 import Header from './Header';
-
-const messages = {
-  "en": {
-    "applicationName": "Reactive I18n Test with React and MobX",
-    "messageLogTitle": "Message Log"
-  },
-  "fi": {
-    "applicationName": "Reaktiivinen internationalisointitesti Reactilla ja MobX:llä",
-    "messageLogTitle": "Viestiloki"
-  }
-};
+import messages from './messages';
 
 const App = observer((props) => (
   <IntlProvider locale={props.store.language} messages={messages[props.store.language]}>
