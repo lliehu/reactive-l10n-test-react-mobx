0da03c98c3983db9937b0b710a887fb7cdb614f2
Lasse Liehu <lasse.liehu@iki.fi>
2019-11-02 22:35:12 +0200

    Tidy LanguageSwitcher up a bit

--- src/LanguageSwitcher.js
+++ src/LanguageSwitcher.js
@@ -1,12 +1,18 @@
 import React from 'react';
 import { observer } from 'mobx-react';
 
-const LanguageSwitcher = observer((props) => (
-  <div>
-    <button onClick={() => {props.store.switchLanguageTo('fi')}}>Suomi</button>
-    <button onClick={() => {props.store.switchLanguageTo('en')}}>English</button>
-    <p>Active language: { props.store.language }</p>
-  </div>
-));
+const LanguageSwitcher = observer((props) => {
+  function switchLanguageTo(language) {
+    props.store.switchLanguageTo(language);
+  }
+
+  return (
+    <div>
+      <button onClick={switchLanguageTo.bind(null, 'fi')}>Suomi</button>
+      <button onClick={switchLanguageTo.bind(null, 'en')}>English</button>
+      <p>Active language: { props.store.language }</p>
+    </div>
+  );
+});
 
 export default LanguageSwitcher;
