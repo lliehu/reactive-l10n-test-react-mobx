abc96b112b3519da0ff89eda5dc00873d6f1f39d
Lasse Liehu <lasse.liehu@iki.fi>
2019-11-03 20:12:18 +0200

    Make title for message log a proper heading

--- src/MessageLog.js
+++ src/MessageLog.js
@@ -2,7 +2,11 @@ import React from 'react';
 import { FormattedMessage } from 'react-intl';
 
 const MessageLog = (props) => (
-  <div><FormattedMessage id='messageLogTitle'/></div>
+  <div>
+    <h2>
+      <FormattedMessage id='messageLogTitle'/>
+    </h2>
+  </div>
 );
 
 export default MessageLog;
