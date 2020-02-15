c05328c9535e088f5f8df21bfc3b046f52daaf59
Lasse Liehu <lasse.liehu@iki.fi>
2020-02-15 22:10:06 +0200

    Fix test

--- src/App.test.js
+++ src/App.test.js
@@ -3,14 +3,16 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import LanguageStore from './LanguageStore';
 import MessageLogStore from './MessageLogStore';
+import MapStore from './MapStore';
 
 const languageStore = new LanguageStore();
 const messageLogStore = new MessageLogStore();
+const mapStore = new MapStore();
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
   ReactDOM.render(
-    <App store={languageStore} messageLogStore={messageLogStore}/>,
+    <App store={languageStore} messageLogStore={messageLogStore} mapStore={mapStore} />,
     div
   );
   ReactDOM.unmountComponentAtNode(div);
