6ce562cbe35b0df1b16339a0b6c3f2191549ac34
Lasse Liehu <lasse.liehu@iki.fi>
2020-02-15 15:40:57 +0200

    Add Material-UI and style language switcher using it

--- src/LanguageSwitcher.js
+++ src/LanguageSwitcher.js
@@ -1,6 +1,19 @@
 import React from 'react';
 import { observer } from 'mobx-react';
 import { FormattedMessage } from 'react-intl';
+import Button from '@material-ui/core/Button';
+import ButtonGroup from '@material-ui/core/ButtonGroup';
+
+const languages = [
+  {
+    code: 'en',
+    nativeName: 'English'
+  },
+  {
+    code: 'fi',
+    nativeName: 'Suomi'
+  }
+];
 
 const LanguageSwitcher = observer((props) => {
   function switchLanguageTo(language) {
@@ -14,17 +27,20 @@ const LanguageSwitcher = observer((props) => {
   return (
     <div>
       <h2><FormattedMessage id='languageSwitcherTitle'/></h2>
-      <button onClick={switchLanguageTo.bind(null, 'fi')}>Suomi</button>
-      <button onClick={switchLanguageTo.bind(null, 'en')}>English</button>
-      <button onClick={enableAutomaticUILanguage}>
-        <FormattedMessage id='automaticLanguage'/>
-      </button>
-      <p>
-        <FormattedMessage
-          id='currentLanguage'
-          values={{language: props.store.language}}
-        />
-      </p>
+      <ButtonGroup color="primary">
+        <Button onClick={enableAutomaticUILanguage} variant={ !props.store.manuallySelectedLanguage ? 'contained' : 'outlined' } disableElevation>
+          <FormattedMessage id='automaticLanguage'/>
+        </Button>
+        {languages.map(language => (
+          <Button
+            onClick={switchLanguageTo.bind(null, language.code)}
+            variant={ !!props.store.manuallySelectedLanguage && props.store.language === language.code ? 'contained' : 'outlined'}
+            disableElevation
+          >
+            {language.nativeName}
+          </Button>
+        ))}
+      </ButtonGroup>
     </div>
   );
 });
