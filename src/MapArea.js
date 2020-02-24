15da6f4554bdfa861d715ef5fcec23d8aaa36190
Lasse Liehu <lasse.liehu@iki.fi>
2020-02-24 09:39:39 +0200

    Manage marker lifecycle in JSX

--- src/MapArea.js
+++ src/MapArea.js
@@ -31,33 +31,28 @@ const MapArea = observer((props) => {
   const zoomInTitle = formatMessage({id: 'zoomInTitle'});
   const zoomOutTitle = formatMessage({id: 'zoomOutTitle'});
 
-  // TODO Is using a ref here really a good idea?
-  const mapRef = React.createRef();
-
   function addMarker(event) {
     props.messageLogStore.addLogMessage('markerAddedMessage', {
       position: event.latlng.toString()
     })
-    let marker = leaflet.marker(event.latlng)
-    marker.bindPopup(event.latlng.toString())
-    marker.addTo(mapRef.current.leafletElement)
+    props.store.addMarker(event.latlng)
   }
 
   return (
     <div>
       <h2><FormattedMessage id='mapTitle'/></h2>
       <Map center={props.store.center} zoom={mapState.zoom} zoomControl={false}
-        onClick={addMarker} ref={mapRef}
+        onClick={addMarker}
       >
         <TileLayer
           attribution='&amp;copy <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
         />
-        <Marker position={position}>
-          <Popup>
-            A pretty CSS3 popup. <br /> Easily customizable.
-          </Popup>
-        </Marker>
+        { props.store.markerList.map((marker, index) => (
+          <Marker key={index} position={marker}>
+            <Popup>Foobar</Popup>
+          </Marker>
+        )) }
         {/* Using key forces remounting of ZoomControl when zoomInTitle or zoomOutTitle change. */}
         <ZoomControl zoomInTitle={zoomInTitle} zoomOutTitle={zoomOutTitle} key={zoomInTitle + '|' + zoomOutTitle}/>
       </Map>
--- src/MapStore.js
+++ src/MapStore.js
@@ -2,11 +2,16 @@ import { observable, decorate } from 'mobx';
 
 class MapStore {
   center = [61.45, 23.85]
+  markerList = []
   setCenter(latlng) {
     this.center = latlng;
   }
+  addMarker(marker) {
+    this.markerList.push(marker);
+  }
 }
 decorate(MapStore, {
-  center: observable
+  center: observable,
+  markerList: observable
 });
 export default MapStore;
