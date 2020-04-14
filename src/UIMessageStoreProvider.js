d1cbecd04adbec2ca863e8d9beb35ebb0de458dd
Lasse Liehu <lasse.liehu@iki.fi>
2020-04-15 00:40:39 +0300

    Add missing file

---
+++ src/UIMessageStoreProvider.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const storeContext = React.createContext(null)
+
+export const UIMessageStoreProvider = ({ store, children }) => {
+  return <storeContext.Provider value={store}>{children}</storeContext.Provider>
+}
+
+export const useStore = () => {
+  const store = React.useContext(storeContext)
+  if (!store) {
+    // this is especially useful in TypeScript so you don't need to be checking for null all the time
+    throw new Error('useStore must be used within a StoreProvider.')
+  }
+  return store
+}
